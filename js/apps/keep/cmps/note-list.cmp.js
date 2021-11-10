20f501006fa6c66b5ddce6cb703a2aa8bedae5d5
Roy Gil <gilro79@gmail.com>
2021-11-10 20:56:23 +0200

    minor commit

--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -7,15 +7,12 @@ export default {
     props: ['notes'],
     template: `
         <section class="note-list">
-            <div v-for="note in notes" :key="note.id" @click="select(note.id)" class="note-preview-container">
+            <div v-for="note in notes" :key="note.id" class="note-preview-container">
             <note-preview :note="note" />
             </div>
         </section>
     `,
     methods: {
-        select(bookId){
-            // this.$router.push('/book/' + bookId)
-            // this.$emit('selected', book);
-        },
+        
     }
 }
