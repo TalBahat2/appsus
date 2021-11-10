9d592bc86f216f64f66e21fc3883a799a535be95
Roy Gil <gilro79@gmail.com>
2021-11-10 15:46:28 +0200

    first change- logog in header

--- js/cmps/app-header.cmps.js
+++ js/cmps/app-header.cmps.js
@@ -4,7 +4,7 @@ export default {
     template: `
         <section class="app-header">
             <div class="header-elements flex space-between align-center main-layout">
-                <div class="logo">logo</div>
+                <div class="logo">logog</div>
                 <nav>
                     <router-link to="/">Home</router-link>
                     <router-link to="/about">About</router-link>
